69f609f61d0fbe3ca0e6ca25015e9c6ffc0002a6
aratner <aratner92@gmail.com>
2017-10-11 00:04:57 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,8 +3,6 @@ var express = require('express');
 var contentDisposition = require('content-disposition');
 var pkg = require( path.join(__dirname, 'package.json') );
 
-var scan = require('./scan');
-
 // Parse command line options
 
 var program = require('commander');
@@ -18,11 +16,10 @@ program
 // add the 'files/' prefix to all files and folders, so that
 // download links point to our /files route
 
-var tree = scan('.', 'files');
-
 // Ceate a new express app
 
 var app = express();
+var port;
 
 // Serve static files from the frontend folder
 
@@ -41,15 +38,14 @@ app.use('/files', express.static(process.cwd(), {
     }
 }));
 
-// This endpoint is requested by our frontend JS
-
-app.get('/scan', function(req,res){
-    res.send(tree);
-});
-
 // Everything is setup. Listen on the port.
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
 console.log('Files is running on port ' + port);
+
+
+
+
