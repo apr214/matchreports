ef31cceb665019a870a05dd2b91d6acdf4cc3218
aratner <aratner92@gmail.com>
2017-10-11 12:58:54 -0400

    Add files via upload

---
+++ tableSize.js
@@ -0,0 +1,10 @@
+function tableSize() {
+
+var a = parseInt(document.getElementById("num_incident").value);
+        var ch = document.getElementById("ma");
+
+        for (i = 0; i < a; i++) {
+            var input = document.createElement("input");
+            ma.appendChild(input);
+        }
+    }
