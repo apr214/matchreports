f069743bf652605ad3b71222e749757d1455b162
aratner <aratner92@gmail.com>
2017-10-13 15:22:59 -0400

    Add files via upload

--- js/addRow.js
+++ js/addRow.js
@@ -6,7 +6,7 @@ function addRow() {
     $('#tableBody').append('<tr>' +
         '<td>' + rowNum + '</td>' +
         //'<td><input type="text" id="inc_type' + rowNum + '"/></td>' +
-        '<td><select names="type" id="inc_type' + rowNum + '" class="dropdown">/td><option value="RED">RED</option><option value="PENALTY">PENALTY</option><option value="GOAL">GOAL</option><option value="Mistaken Identy">Mistaken Identy</option>' +
+        '<td><select names="type" id="inc_type' + rowNum + '" class="dropdown">/td><option value="RED">RED</option><option value="PENALTY">PENALTY</option><option value="GOAL">GOAL</option><option value="Mistaken Identy">Mistaken Identity</option>' +
         '<td><select names="Confirms" id="confirm' + rowNum + '" class="dropdown">/td><option value="Confirm">Confirm</option><option value="Overrule">Overrule</option>' +
         '<td><input type="text" id="description' + rowNum + '"/></td>' +
         '<td><select names="Training" class="dropdown" id="training' + rowNum + '">/td><option value="NO">NO</option><option value="YES">YES</option>'+
