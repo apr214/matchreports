ccb86f0f609cfe7fa1a30eacd221797884de19a4
aratner <aratner92@gmail.com>
2017-11-01 10:40:16 -0700

    Add files via upload

--- js/addRow.js
+++ js/addRow.js
@@ -7,7 +7,7 @@ function addRow() {
         '<td>' + rowNum + '</td>' +
         //'<td><input type="text" id="inc_type' + rowNum + '"/></td>' +
         '<td><select names="type" id="inc_type' + rowNum + '" class="dropdown">/td><option value="RED">RED</option><option value="PENALTY">PENALTY</option><option value="GOAL">GOAL</option><option value="Mistaken Identy">Mistaken Identity</option>' +
-        '<td><select names="Confirms" id="confirm' + rowNum + '" class="dropdown">/td><option value="Confirm">Confirm</option><option value="Overrule">Overrule</option>' +
+        '<td><select names="Confirms" id="confirm' + rowNum + '" class="dropdown">/td><option value="Confirm">Confirm</option><option value="Overrule">Overrule</option><option value="Not Given">Not Given</option><option value="Given">Given</option>' +
         '<td><input type="text" id="description' + rowNum + '"/></td>' +
         '<td><select names="Training" class="dropdown" id="training' + rowNum + '">/td><option value="NO">NO</option><option value="YES">YES</option>'+
         '<td><input type="text" id="varfeed' + rowNum + '"/></td>' +
