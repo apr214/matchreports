f1ff7afe265671e6635d98b52a3e402a3b2e6533
aratner <aratner92@gmail.com>
2018-06-30 15:59:07 -0700

    Add files via upload

---
+++ js/submitIssuesForm.js
@@ -0,0 +1,14 @@
+function submitIssuesForm(formId) {
+    const script = 'https://script.google.com/macros/s/AKfycbxvHYyLBn7YWkZqXutcOkOhrgEbv_xn8YbW-3Nbwo5AK_vpQXM/exec'
+    const form = document.forms[formId];
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(script, {
+                method: 'POST',
+                body: new FormData(form)
+            })
+            .then(response => console.log('Success!', response))
+            .catch(error => console.error('Error!', error.message))
+    })
+}
