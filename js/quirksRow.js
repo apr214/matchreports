0e8fa0fd07c92b8a749b75434f00c744f3c5662d
aratner <aratner92@gmail.com>
2018-06-30 08:36:46 -0700

    Add files via upload

--- js/quirksRow.js
+++ js/quirksRow.js
@@ -5,9 +5,9 @@ function addQuirk() {
     rowNum = $('#quirksBody > tr').length + 1;
     $('#quirksBody').append('<tr>' +
         //'<td><input type="text" id="inc_type' + rowNum + '"/></td>' +
-        '<td><textarea rows="8" cols="75" id="quirk" placeholder="Quirk/Issue"' + rowNum + '"></textarea></td>' +
-        '<td><select names="Hardware/Software" id="hardsoft" placeholder="Hardware/Software"' + rowNum + '" class="dropdown">/td><option value="Hardware">Hardware</option><option value="Software">Software</option>' +
-        '<td><textarea rows="10" cols="90" id="qnotes" wrap="hard" placeholder="Improvements Made"></textarea></td>"'
+        rowNum + '<td><textarea rows="8" cols="50" id="quirk" placeholder="Quirk/Issue"' + rowNum + '"></textarea></td>' +
+        '<td><select names="Hardware/Software" id="hardsoft" placeholder="Hardware/Software"' + rowNum + '" class="dropdown"><option value="Hardware">Hardware</option><option value="Software">Software</option><option value="Audio">Audio</option></select><select name="Location" id="hardsoft" class="dropdown" value="Location?"><option value="TT">Location?</option><option value="VOR">VOR</option><option value="IO">IO</option><option value="VIOR">VOR/IO</option></select> <input type="text" name="Reported By?" placeholder="Reported By?" /></td>' +
+        '<td><textarea rows="10" cols="40" id="qnotes" wrap="hard" placeholder="Improvements Made"></textarea></td>"' + '<td><textarea rows="10" cols="40" id="qnotes" wrap="hard" placeholder="Further Action"></textarea></td>"' +'<td><select name="Ticket Closed" id="hardsoft" class="dropdown" value="Location?"><option value="Y">Y</option><option value="N">N</option></select></td>'
     );
 
 }
